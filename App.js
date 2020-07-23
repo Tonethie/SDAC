c9b8baa0563794b3f0d88c35eacb8621af40949f
Julia Johnson <55238578+juliajohnsonmelo@users.noreply.github.com>
2020-07-22 22:10:34 -0300

    inicio firebase
    inicio da configuração do firebase

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import { Button, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 // import the different screens
-import Home from './pages/Home'
-import SignUp from './pages/SignUp'
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 // create our app's navigation stack
+import * as firebase from 'firebase/app';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyCnS8w3hCRJsNPZwyWOdXrAMY9Ep2BIHMA',
+  authDomain: 'sdac-6dd65.firebaseapp.com',
+  databaseURL: 'https://sdac-6dd65.firebaseio.com',
+  projectId: 'sdac-6dd65',
+  storageBucket: 'sdac-6dd65.appspot.com',
+  messagingSenderId: '1016413694584',
+  appId: '1:1016413694584:web:0db0f2566149ebdf5a1b8d',
+  measurementId: 'G-HEZBDNPWFW',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
-      <Stack.Screen options={{headerShown: false}} name="SignUp" component={SignUp} />
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="Home"
+        component={Home}
+      />
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="SignUp"
+        component={SignUp}
+      />
     </Stack.Navigator>
   );
 }
