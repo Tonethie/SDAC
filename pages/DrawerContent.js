f83f861b0cbff0b04fb4b89bd5b1ef8b297fc458
Tonethie <luiz_fcarvalho@hotmail.com>
2020-07-24 22:22:14 -0300

    DrawerContent

--- pages/DrawerContent.js
+++ pages/DrawerContent.js
@@ -1,14 +1,181 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, StyleSheet} from 'react-native';
+import {
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableRipple,
+    Switch
+} from 'react-native-paper';
 import {
     DrawerContentScrollView,
     DrawerItem
 } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function DrawerContent(props){
+
+    const[isDarkTheme, setIsDarkTheme] = React.useState(false);
+
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    }
+
     return(
-        <View>
-            <Text>Um Teste</Text>
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.drawerContent}>
+                    <View style={styles.userInfoSection}>
+                        <View style={{flexDirection:'row', marginTop: 15}}>
+                            <Avatar.Image
+                                source={{
+                                    uri: 'https://http2.mlstatic.com/camiseta-personalizada-desenho-anime-avatar-D_NQ_NP_6167-MLB5032845448_092013-F.jpg'
+                                }}
+                                size={75}
+                            />
+                            <View style={{marginLeft:15, flexDirection:'column'}}>
+                                <Title style={styles.title}>Avatar Aang</Title>
+                                <Caption style={styles.caption}>Last Airbender</Caption>
+                            </View>
+                        </View>
+                        <View style={styles.row}>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>80</Paragraph>
+                                <Caption style={styles.caption}>Device Sync</Caption>
+                            </View>
+                        </View>
+                    </View>
+                    <Drawer.Section style={styles.drawerSection}>
+                        <DrawerItem
+                            icon={({color,size}) =>(
+                                <Icon
+                                name="home-outline"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Início"
+                            onPress={() => {props.navigation.navigate('Home')}}
+                        />
+                        <DrawerItem
+                            icon={({color,size}) =>(
+                                <Icon
+                                name="book-outline"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Lista BT"
+                            onPress={() => {props.navigation.navigate('ListBT')}}
+                        />
+                        <DrawerItem
+                            icon={({color,size}) =>(
+                                <Icon
+                                name="account-outline"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Perfil"
+                            onPress={() => {}}
+                        />
+                        <DrawerItem
+                            icon={({color,size}) =>(
+                                <Icon
+                                name="cellphone-settings"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Configurações"
+                            onPress={() => {props.navigation.navigate('Settings')}}
+                        />
+                        <DrawerItem
+                            icon={({color,size}) =>(
+                                <Icon
+                                name="account-check-outline"
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Suporte"
+                            onPress={() => {}}
+                        />
+                    </Drawer.Section>
+                    <Drawer.Section title="Preferência">
+                        <TouchableRipple onPress={() => {toggleTheme()}}>
+                            <View style={styles.preference}>
+                                <Text>Dark Theme</Text>
+                                <View pointerEvents="none">
+                                    <Switch value={isDarkTheme} />
+                                </View>
+                            </View>
+                        </TouchableRipple>
+                    </Drawer.Section>
+                </View>
+            </DrawerContentScrollView>
+            <Drawer.Section style={styles.bottomDrawerSection}>
+                <DrawerItem
+                    icon={({color,size}) =>(
+                        <Icon
+                        name="exit-to-app"
+                        color={color}
+                        size={size}
+                        />
+                    )}
+                    label="Sair"
+                    onPress={() => {}}
+                />
+            </Drawer.Section>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    drawerContent: {
+        flex: 1,
+    },
+    userInfoSection: {
+        paddingLeft: 20,
+    },
+    title: {
+        fontSize: 16,
+        marginTop: 3,
+        fontWeight: 'bold',
+    },
+    caption: {
+        fontSize: 14,
+        lineHeight: 14,
+    },
+    row: {
+        marginTop: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    section: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 15,
+    },
+    paragraph: {
+        fontWeight: 'bold',
+        marginRight: 3,
+    },
+    drawerSection: {
+        marginTop: 15,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#f4f4f4',
+        borderTopWidth: 1,
+    },
+    preference: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+    },
+});
--- pages/home.routes.js
+++ pages/home.routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/Ionicons'
 import ListBT from './ListBT';
 import Home from './Home';
 import Settings from './Settings';
+import { DrawerContent } from './DrawerContent';
 Icon.loadFont();
 
 const Drawer = createDrawerNavigator();
@@ -16,8 +17,8 @@ const HomeScreen = ({navigation}) => (
             headerShown: true,
             title: 'Início',
             headerLeft: () => (
-                <Icon.Button name="menu" size={25}
-                backgroundColor="#009387" onPress={() =>{navigation.openDrawer()}}></Icon.Button>
+                <Icon name="menu-outline" size={25}
+                onPress={() =>{navigation.openDrawer()}}></Icon>
             )
         }} name="Home" component={Home} />
     </Stack.Navigator>
@@ -28,8 +29,8 @@ const ListBTScreen = ({navigation}) => (
             headerShown: true,
             title: 'Lista BT',
             headerLeft: () => (
-                <Icon.Button name="menu" size={25}
-                backgroundColor="#009387" onPress={() =>{navigation.openDrawer()}}></Icon.Button>
+                <Icon name="menu-outline" size={25}
+                onPress={() =>{navigation.openDrawer()}}></Icon>
             )
         }} name="ListBT" component={ListBT} />
     </Stack.Navigator>
@@ -40,8 +41,8 @@ const SettingScreen = ({navigation}) => (
             headerShown: true,
             title: 'Configurações',
             headerLeft: () => (
-                <Icon.Button name="menu" size={25}
-                backgroundColor="#009387" onPress={() =>{navigation.openDrawer()}}></Icon.Button>
+                <Icon name="menu-outline" size={25}
+                onPress={() =>{navigation.openDrawer()}}></Icon>
             )
         }} name="Settings" component={Settings} />
     </Stack.Navigator>
@@ -50,7 +51,7 @@ const SettingScreen = ({navigation}) => (
 
 export default function HomeRoutes () {
     return(
-        <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />}>
             <Drawer.Screen name="Home" component={HomeScreen} />
             <Drawer.Screen name="ListBT" component={ListBTScreen} />
             <Drawer.Screen name="Settings" component={SettingScreen} />
