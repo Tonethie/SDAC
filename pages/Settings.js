524a194b82eef051313895e6a4aebd2e5aae62b7
Tonethie <luiz_fcarvalho@hotmail.com>
2020-08-07 00:42:12 -0300

    Update Settings.js

--- pages/Settings.js
+++ pages/Settings.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { StyleSheet, Image, View, TouchableOpacity, Text, TextInput, TouchableHighlight } from 'react-native';
+import { StyleSheet, Image, View, Picker, Text, TextInput, TouchableHighlight } from 'react-native';
 
 export default class Settings extends React.Component {
 
     render(){
         return(
             <View style={styles.container}>
-                <View style={styles.txtContainer}>
-                    <Text>Você chegou nas Configurações</Text>
+                <View style={styles.campos}>
+                    <Text style={styles.corTexto}>SSID</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Nome do WiFi"
+                        underlineColorAndroid="gray"></TextInput>
+                    <Text style={styles.corTexto}>Mode</Text>
+                    <Picker
+                        style={{ height: 50, width: 300 }}
+                        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+                        <Picker.Item label="54Mbps(802.11g)" value="java" />
+                        <Picker.Item label="JavaScript" value="js" />
+                    </Picker>
+                    <Text style={styles.corTexto}>Tipo de Segurança</Text>
+                    <Picker
+                        style={{ height: 50, width: 300 }}
+                        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+                        <Picker.Item label="WEP" value="java" />
+                        <Picker.Item label="JavaScript" value="js" />
+                    </Picker>
+                    <Text style={styles.corTexto}>Opções de Segurança</Text>
+                    <Picker
+                        style={{ height: 50, width: 300 }}
+                        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+                        <Picker.Item label="Automática" value="java" />
+                        <Picker.Item label="JavaScript" value="js" />
+                    </Picker>
+                    <Text style={styles.corTexto}>Senha</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Senha do WiFi"
+                        secureTextEntry={true}
+                        underlineColorAndroid="gray"></TextInput>
+                    <TouchableHighlight style={styles.loginBtn}>
+                        <Text style={styles.loginTxt}>Salvar</Text>
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.cadastroContainer}>
+                    <TouchableHighlight>
+                        <Text style={{marginTop: 40, color: '#f8606b', fontWeight: 'bold'}}>Cancelar</Text>
+                    </TouchableHighlight>
                 </View>
             </View>
         );
@@ -21,5 +60,40 @@ const styles = StyleSheet.create({
     txtContainer:{
         justifyContent:"center",
         alignItems: "center"
-    }
+    },
+    campos: {
+        justifyContent: 'center',
+        paddingLeft: 50,
+        marginTop: 40,
+    },
+    corTexto: {
+        color: '#f8606b',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop:10,
+    },
+    input: {
+        width: 290,
+        height: 40,
+        borderRadius: 25,
+        paddingLeft: 15,
+    },
+    loginBtn: {
+        width: 290,
+        height: 45,
+        borderRadius: 7,
+        justifyContent: 'center',
+        backgroundColor: '#f8606b',
+        marginTop: 40,
+    },
+    loginTxt: {
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    cadastroContainer: {
+        alignContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
 });
