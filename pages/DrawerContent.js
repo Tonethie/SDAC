3dc66bcaed3b869fad1efdb6ca0969f0d0db13c4
Tonethie <luiz_fcarvalho@hotmail.com>
2020-08-21 22:53:42 -0300

    Update DrawerContent.js

--- pages/DrawerContent.js
+++ pages/DrawerContent.js
@@ -2,14 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet} from 'react-native';
 
 import {
-    Avatar,
-    Title,
-    Caption,
-    Paragraph,
-    Drawer,
-    Text,
-    TouchableRipple,
-    Switch
   Avatar,
   Title,
   Caption,
@@ -19,25 +11,14 @@ import {
   TouchableRipple,
   Switch,
 } from 'react-native-paper';
-import {
-    DrawerContentScrollView,
-    DrawerItem
-} from '@react-navigation/drawer';
 import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as firebase from 'firebase/app';
 import auth from 'firebase/auth';
-import { set } from 'react-native-reanimated';
 
-export function DrawerContent(props){
-
-    const[isDarkTheme, setIsDarkTheme] = React.useState(false);
 export function DrawerContent(props) {
   const [isDarkTheme, setIsDarkTheme] = React.useState(false);
 
-    const toggleTheme = () => {
-        setIsDarkTheme(!isDarkTheme);
-    }
   const toggleTheme = () => {
     setIsDarkTheme(!isDarkTheme);
   };
@@ -174,57 +155,12 @@ export function DrawerContent(props) {
                     onPress={() => {signOutUser();}}
                 />
             </Drawer.Section>
-        </View>
-    );
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    drawerContent: {
-        flex: 1,
-    },
-    userInfoSection: {
-        paddingLeft: 20,
-    },
-    title: {
-        fontSize: 16,
-        marginTop: 3,
-        fontWeight: 'bold',
-    },
-    caption: {
-        fontSize: 14,
-        lineHeight: 14,
-    },
-    row: {
-        marginTop: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    section: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginRight: 15,
-    },
-    paragraph: {
-        fontWeight: 'bold',
-        marginRight: 3,
-    },
-    drawerSection: {
-        marginTop: 15,
-    },
-    bottomDrawerSection: {
-        marginBottom: 15,
-        borderTopColor: '#f4f4f4',
-        borderTopWidth: 1,
-    },
-    preference: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 12,
-        paddingHorizontal: 16,
-    },
-});  drawerContent: {
+  drawerContent: {
     flex: 1,
   },
   userInfoSection: {
