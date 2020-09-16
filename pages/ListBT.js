c94c4a21e0b3449af44bc6095649f92e13348c20
Tonethie <luiz_fcarvalho@hotmail.com>
2020-09-16 19:49:36 -0300

    Deletar bluetooth

--- pages/ListBT.js
+++ pages/ListBT.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Alert, Image, View, Text } from 'react-native';
 import { Card, Title, Paragraph, Button} from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as firebase from 'firebase/app';
 import auth from 'firebase/auth';
 import database from 'firebase/database';
@@ -34,6 +35,11 @@ export default class ListBT extends React.Component {
             this.setState({items:items});
         });
     }
+    deleteUserData(key){
+        let btRem = firebase.database()
+                            .ref('/users/' + firebase.auth().currentUser.uid + '/bt_data/' + key);
+        btRem.remove()
+    }
     
     render(){
         return(
@@ -42,14 +48,33 @@ export default class ListBT extends React.Component {
                 this.state.items.map((item, index)=>{
                     if((item.nome).charAt(0) == 'e'){
                         return (
-                            <Card key={index} onPress={() => Alert.alert("Configurar bluetooth?")}>
+                            <Card key={index}  onPress={() => Alert.alert("Configurar bluetooth?")}>
                                 <Card.Content style={{flexDirection: 'row', alignItems:"center"}}>
                                     <Image
                                         source={{uri: 'https://d338t8kmirgyke.cloudfront.net/icons/icon_pngs/000/004/118/original/smart-tv.png'}}
                                         style={{width:30, height: 30}}
                                         />
                                     <Text>  {item.nome}</Text>
-                                </Card.Content>
+                                    <Button style={{position:"absolute", right: 0}} icon={({black,size}) =>(
+                                        <Icon
+                                        name="dots-horizontal"
+                                        color={"#f8606b"}
+                                        size={size}
+                                        />
+                                    )} onPress={() => Alert.alert("Excluir","Deseja deletar o bluetooth da lista?",
+                                    [
+                                        {
+                                            text: "Sim",
+                                            onPress: () => this.deleteUserData(item.key)
+                                        },
+                                        {
+                                            text:"Cancelar",
+                                            onPress: () => console.log("Cancelado"),
+                                            style: "cancel"
+                                        }
+                                    ],
+                                    )}></Button>
+                                </Card.Content> 
                             </Card>
                         );
 
@@ -62,6 +87,25 @@ export default class ListBT extends React.Component {
                                     style={{width:30, height: 30}}
                                     />
                                 <Text>  {item.nome}</Text>
+                                <Button style={{position:"absolute", right: 0}} icon={({black,size}) =>(
+                                        <Icon
+                                        name="dots-horizontal"
+                                        color={"#f8606b"}
+                                        size={size}
+                                        />
+                                    )} onPress={() => Alert.alert("Excluir","Deseja deletar o bluetooth da lista?",
+                                    [
+                                        {
+                                            text: "Sim",
+                                            onPress: () => this.deleteUserData(item.key)
+                                        },
+                                        {
+                                            text:"Cancelar",
+                                            onPress: () => console.log("Cancelado"),
+                                            style: "cancel"
+                                        }
+                                    ],
+                                    )}></Button>
                             </Card.Content>
                         </Card>
                     );
